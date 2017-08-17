c5d51a3425f2c0e2aa7067c42467e3b15394c967
fewizz <hazeevaidar@gmail.com>
2017-08-17 16:30:33 +0300

    Fix loading on 1.12, Closes #1

--- src/main/java/atheria/fewizz/trade/Trade.java
+++ src/main/java/atheria/fewizz/trade/Trade.java
@@ -38,12 +38,12 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(name = Trade.NAME, modid = Trade.MODID, version = Trade.VERSION)
+@Mod(name = Trade.NAME, modid = Trade.MODID, version = Trade.VERSION, acceptedMinecraftVersions = "1.12, 1.12.1")
 public class Trade {
 	public static final Trade INSTANCE = new Trade();
 	public static final String NAME = "Trade";
 	public static final String MODID = "trade";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	public static final SimpleNetworkWrapper NETWORK_WRAPPER = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
 
 	// For server world, key = player, value = requested players
