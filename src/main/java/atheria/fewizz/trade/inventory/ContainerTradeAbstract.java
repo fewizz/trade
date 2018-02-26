7d20a3e52141cc49b774059aecce052ba1deacc5
fewizz <hazeevaidar@gmail.com>
2018-02-26 12:05:14 +0300

    Like vanilla

--- src/main/java/atheria/fewizz/trade/inventory/ContainerTradeAbstract.java
+++ src/main/java/atheria/fewizz/trade/inventory/ContainerTradeAbstract.java
@@ -22,24 +22,24 @@ public abstract class ContainerTradeAbstract extends Container {
 		this.otherPlayerName = otherPlayerName;
 		this.playerName = player.getName();
 	}
-	
+
 	public void initSlots() {
 		for (int h = 0; h < 3; h++) {
 			for (int w = 0; w < 9; w++) {
 				addSlotToContainer(new Slot(inventoryPlayer, h * 9 + w + 9, w * 18 + 6, 92 + h * 18));
 			}
 		} // 0 - 26
-		
+
 		for (int x = 0; x < 9; x++) {
 			addSlotToContainer(new Slot(inventoryPlayer, x, x * 18 + 6, 150));
 		} // 27 - 35
-		
+
 		for (int x = 0; x < 3; x++) { // 36 - 53
 			for (int y = 0; y < 3; y++) {
 				addSlotToContainer(new Slot(inventoryTrade, x * 3 + y, x * 18 + 14, 19 + y * 18) {
 					@Override
 					public void onSlotChanged() {
-						if(getTradeState() == TradeState.READY)
+						if (getTradeState() == TradeState.READY)
 							setTradeState(TradeState.NOT_READY);
 						super.onSlotChanged();
 					}
@@ -61,29 +61,39 @@ public abstract class ContainerTradeAbstract extends Container {
 
 	@Override
 	public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
-        Slot slot = this.inventorySlots.get(index);
-        ItemStack stack = slot.getStack();
+		ItemStack itemstack = ItemStack.EMPTY;
+		Slot slot = this.inventorySlots.get(index);
 
-        //if (slot == null || stack == ItemStack.EMPTY || (index & 0x1) == 1){
-        //	return ItemStack.EMPTY;
-        //}
-        
-        if(index >= 27 && index <= 35) {
-        	mergeItemStack(stack, 0, 27, false);
-        }
-        else if(index <= 35) {
-        	mergeItemStack(stack, 36, 54, false);
-        }
-        else if(index >= 36 && index < 54) {
-        	mergeItemStack(stack, 0, 36, false);
-        }
-        
-        if(slot != null)
-        	slot.onSlotChanged();
-        
-        return ItemStack.EMPTY;
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+
+			if (index >= 27 && index <= 35) {
+				if (!mergeItemStack(itemstack1, 0, 27, false))
+					return ItemStack.EMPTY;
+			} else if (index <= 35) {
+				if (!mergeItemStack(itemstack1, 36, 54, false))
+					return ItemStack.EMPTY;
+			} else if (index >= 36 && index < 54) {
+				if (!mergeItemStack(itemstack1, 0, 36, false))
+					return ItemStack.EMPTY;
+			}
+
+			if (itemstack1.isEmpty())
+				slot.putStack(ItemStack.EMPTY);
+
+			else
+				slot.onSlotChanged();
+
+			if (itemstack1.getCount() == itemstack.getCount())
+				return ItemStack.EMPTY;
+
+			slot.onTake(playerIn, itemstack1);
+		}
+
+		return itemstack;
 	}
-	
+
 	public void setTradeState(TradeState state) {
 		this.tradeState = state;
 
@@ -101,7 +111,7 @@ public abstract class ContainerTradeAbstract extends Container {
 	public TradeState getTradeState() {
 		return tradeState;
 	}
-	
+
 	@Override
 	public boolean canInteractWith(EntityPlayer playerIn) {
 		return true;
